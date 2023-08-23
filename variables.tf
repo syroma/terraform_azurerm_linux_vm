f52d9f322d0bc72f4dac12ee20a6ddc9038fd3af
Osiris Roman <osiris.roman@protonmail.com>
2023-08-22 22:53:55 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "prefix" {}
+variable "address_space" {}
+variable "address_prefixes" {}
+variable "account_kind" {}
+variable "account_tier" {}
+variable "account_replication_type" {}
+variable "admin_username" {}
+variable "vm_size" {}
+variable "disk_caching" {}
+variable "storage_account_type" {}
+
+variable "location" {}
+
+variable "container_access_type" {}
+
+variable "ip_name" {}
+variable "private_ip_address_allocation" {}
